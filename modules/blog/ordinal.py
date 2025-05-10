73b9aaa5d344bbe77317f6c3b0a2197418aaa760
Ram Kattubadi <137182701+kattubadiram@users.noreply.github.com>
2025-05-09 17:04:46 -0700

    Delete modules directory

--- modules/blog/ordinal.py
+++
@@ -1,7 +0,0 @@
-# utils/ordinal.py
-
-def ordinal(n: int) -> str:
-    """Return the ordinal representation of an integer (e.g., 1 -> '1st', 2 -> '2nd')."""
-    if 11 <= (n % 100) <= 13:
-        return f"{n}th"
-    return f"{n}{ {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, 'th') }"
